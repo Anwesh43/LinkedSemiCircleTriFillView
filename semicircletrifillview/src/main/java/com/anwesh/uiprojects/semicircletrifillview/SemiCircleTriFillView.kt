9fa1d634f311b5a431871cf8ca9c5b61c5188ac1
Anwesh43 <anweshthecool0@gmail.com>
2020-10-18 04:42:13 +0530

    adding SCTFNode class

--- semicircletrifillview/src/main/java/com/anwesh/uiprojects/semicircletrifillview/SemiCircleTriFillView.kt
+++ semicircletrifillview/src/main/java/com/anwesh/uiprojects/semicircletrifillview/SemiCircleTriFillView.kt
@@ -134,5 +134,46 @@ class SemiCircleTriFillView(ctx : Context) : View(ctx) {
                 }
             }
         }
+
+        data class SCTFNode(private var i : Int, val state : State = State()) {
+
+            private var next : SCTFNode? = null
+            private var prev : SCTFNode? = null
+
+            init {
+                addNeighbor()
+            }
+
+            fun addNeighbor() {
+                if (i < colors.size - 1) {
+                    next = SCTFNode(i + 1)
+                    next?.prev = this
+                }
+            }
+
+            fun draw(canvas : Canvas, paint : Paint) {
+                canvas.drawSCTFNode(i, state.scale, paint)
+            }
+
+            fun update(cb : (Float) -> Unit) {
+                state.update(cb)
+            }
+
+            fun startUpdating(cb : () -> Unit) {
+                state.startUpdating(cb)
+            }
+
+            fun getNext(dir : Int, cb : () -> Unit) : SCTFNode {
+                var curr : SCTFNode? = prev
+                if (dir == 1) {
+                    curr = next
+                }
+                if (curr != null) {
+                    return curr
+                }
+                cb()
+                return this
+            }
+        }
     }
 }
