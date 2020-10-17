4208b7011126e3761d6cad55de99c159a5bc89f3
Anwesh43 <anweshthecool0@gmail.com>
2020-10-18 04:44:17 +0530

    adding SemiCircleTriFill class

--- semicircletrifillview/src/main/java/com/anwesh/uiprojects/semicircletrifillview/SemiCircleTriFillView.kt
+++ semicircletrifillview/src/main/java/com/anwesh/uiprojects/semicircletrifillview/SemiCircleTriFillView.kt
@@ -175,5 +175,28 @@ class SemiCircleTriFillView(ctx : Context) : View(ctx) {
                 return this
             }
         }
+
+        data class SemiCircleTriFill(var i : Int) {
+
+            private var curr : SCTFNode = SCTFNode(0)
+            private var dir : Int = 1
+
+            fun draw(canvas : Canvas, paint : Paint) {
+                curr.draw(canvas, paint)
+            }
+
+            fun update(cb : (Float) -> Unit) {
+                curr.update {
+                    curr = curr.getNext(dir) {
+                        dir *= -1
+                    }
+                    cb(it)
+                }
+            }
+
+            fun startUpdating(cb : () -> Unit) {
+                curr.startUpdating(cb)
+            }
+        }
     }
 }
